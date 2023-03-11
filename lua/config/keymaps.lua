a7462347d3dde4d74865b0636797f24ebf5bf588
Julian V <jvesper95@gmail.com>
2023-03-11 02:57:02 +0100

    Add command mode navigation keymaps (<C-j>, <C-k>)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<leader>L", "<cmd>:Lazy<cr>", { desc = "Lazy" })
-
 -- Forbidden keymap
 vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("n", "<leader>L", "<cmd>:Lazy<cr>", { desc = "Lazy" })
+
+vim.keymap.set("c", "<C-j>", "<C-n>")
+vim.keymap.set("c", "<C-k>", "<C-p>")
