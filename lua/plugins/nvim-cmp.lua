dc403cffab153c523859eb8576283fbca4e29236
Julian V <jvesper95@gmail.com>
2023-03-12 11:46:53 +0100

    Add lspkind to nvim-cmp and set maxwidth for cmp window

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,14 +1,25 @@
 return {
   "hrsh7th/nvim-cmp",
+  dependencies = { "onsails/lspkind.nvim" },
   opts = function(_, opts)
     local cmp = require("cmp")
-    opts.mapping = vim.tbl_extend(
-      "force",
-      opts.mapping,
-      {
-        ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-      }
-    )
+    local lspkind = require("lspkind")
+
+    opts.mapping = vim.tbl_extend("force", opts.mapping, {
+      ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+      ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+    })
+    opts.formatting = {
+      format = lspkind.cmp_format({
+        mode = "symbol_text",
+        maxwidth = 60,
+        ellipsis_char = "...",
+
+        symbol_map = require("lazyvim.config").icons.kinds,
+        before = function(_, vim_item)
+          return vim_item
+        end,
+      }),
+    }
   end,
 }
