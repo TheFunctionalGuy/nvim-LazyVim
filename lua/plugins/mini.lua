e16b15b8d7f10505cb3277e260028242be9e9939
Julian V <jvesper95@gmail.com>
2023-03-13 13:48:31 +0100

    Replace mini.pairs by nvim-autopairs

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,10 +1,16 @@
 return {
-  "echasnovski/mini.comment",
-  opts = {
-    mappings = {
-      comment = "<leader>cc",
-      comment_line = "<leader>ccl",
-      textobject = "<leader>cc",
+  {
+    "echasnovski/mini.comment",
+    opts = {
+      mappings = {
+        comment = "<leader>cc",
+        comment_line = "<leader>ccl",
+        textobject = "<leader>cc",
+      },
     },
+  },
+  {
+    "echasnovski/mini.pairs",
+    cond = false,
   },
 }
