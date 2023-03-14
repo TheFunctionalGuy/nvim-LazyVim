9880f475aa3ce5e964f78a8827d18fdca7d3bec3
Julian Vesper <julian.vesper@ruhr-uni-bochum.de>
2023-03-14 00:33:43 +0000

    Add working DAP-config which utilizes launch.json

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -63,21 +63,8 @@ return {
               -- detached = false,
             },
           }
-          dap.configurations.cpp = {
-            {
-              name = "Launch file",
-              type = "codelldb",
-              request = "launch",
-              program = function()
-                return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-              end,
-              cwd = "${workspaceFolder}",
-              stopOnEntry = false,
-            },
-          }
-
-          dap.configurations.c = dap.configurations.cpp
-          dap.configurations.rust = dap.configurations.cpp
+          -- Use launch.json
+          require("dap.ext.vscode").load_launchjs(nil, { codelldb = { "c", "cpp", "rust" } })
         end,
       },
     },
