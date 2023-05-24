1594ce741e0b255febd068df40a02bd1f990dd80
Julian Vesper <jvesper95@gmail.com>
2023-05-24 03:51:46 +0200

    Add oil.nvim plugin + config

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,15 @@
+return {
+  "stevearc/oil.nvim",
+  -- stylua: ignore
+  keys = {
+    { "-", function() require("oil").open() end, desc = "Open parent directory" },
+  },
+  opts = {
+    keymaps = {
+      ["<C-h>"] = false,
+      ["sj"] = "actions.select_split",
+      ["<C-s>"] = false,
+      ["sl"] = "actions.select_vsplit",
+    },
+  },
+}
