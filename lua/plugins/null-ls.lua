929442e0986d81437ffe00ca879e369f4a347df6
Julian Vesper <julian.vesper@ruhr-uni-bochum.de>
2023-06-28 14:27:34 +0200

    Add black formatter config for null-ls

---
+++ lua/plugins/null-ls.lua
@@ -0,0 +1,7 @@
+return {
+  "jose-elias-alvarez/null-ls.nvim",
+  opts = function(_, opts)
+    local null_ls = require("null-ls")
+    opts.sources = vim.list_extend(opts.sources, { null_ls.builtins.formatting.black })
+  end,
+}
