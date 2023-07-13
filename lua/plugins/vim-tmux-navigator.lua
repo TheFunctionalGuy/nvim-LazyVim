9133d7291758c9c0bc02e5f7e206ffc99c2b41fc
Julian Vesper <jvesper95@gmail.com>
2023-07-13 12:15:10 +0200

    Remove oil.nvim
    Also add key descriptions to vim-tmux-navigator.lua

--- lua/plugins/vim-tmux-navigator.lua
+++ lua/plugins/vim-tmux-navigator.lua
@@ -2,10 +2,10 @@ return {
   "christoomey/vim-tmux-navigator",
   -- stylue: ignore
   keys = {
-    { "<C-h>", "<cmd>TmuxNavigateLeft<CR>" },
-    { "<C-j>", "<cmd>TmuxNavigateDown<CR>" },
-    { "<C-k>", "<cmd>TmuxNavigateUp<CR>" },
-    { "<C-l>", "<cmd>TmuxNavigateRight<CR>" },
-    { "<C-\\>", "<cmd>TmuxNavigatePrevious<CR>" },
+    { "<C-h>", "<cmd>TmuxNavigateLeft<CR>", desc = "Go to left window" },
+    { "<C-j>", "<cmd>TmuxNavigateDown<CR>", desc = "Go to lower window" },
+    { "<C-k>", "<cmd>TmuxNavigateUp<CR>", desc = "Go to upper window" },
+    { "<C-l>", "<cmd>TmuxNavigateRight<CR>", desc = "Go to right window" },
+    { "<C-\\>", "<cmd>TmuxNavigatePrevious<CR>", desc = "Go to previous tmux pane" },
   },
 }
