17d28d275a94d734242545584cad2d117f208d7d
Julian Vesper <jvesper95@gmail.com>
2023-07-13 12:15:42 +0200

    Use new push/pull keymaps

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,8 +3,8 @@ return {
   cmd = "Git",
   keys = {
     { "<leader>gj", "<cmd>Git<CR>", desc = "Open Fugitive" },
-    { "<leader>gP", "<cmd>Git pull<CR>", desc = "Git pull" },
-    { "<leader>gp", "<cmd>Git push<CR>", desc = "Git push" },
+    { "<leader>gp", "<cmd>Git pull<CR>", desc = "Git pull" },
+    { "<leader>gP", "<cmd>Git push<CR>", desc = "Git push" },
     { "<leader>gf", "<cmd>Git fetch<CR>", desc = "Git fetch" },
   },
 }
