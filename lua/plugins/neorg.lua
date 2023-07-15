f39b86d492102793cd02b0809ea01b51c1b83e46
Julian Vesper <jvesper95@gmail.com>
2023-07-15 15:33:02 +0200

    Fix bug in neorg config and add back dirman config

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -3,13 +3,21 @@ return {
   build = ":Neorg sync-parsers",
   dependencies = { "nvim-lua/plenary.nvim" },
   cmd = "Neorg",
-  event = "BufEnter *.norg",
+  ft = "norg",
   config = function()
     require("neorg").setup({
       load = {
         ["core.defaults"] = {}, -- Loads default behaviour
         ["core.concealer"] = {}, -- Adds pretty icons to your documents
-        -- core.dirman config can be found in: neorg-dirman.lua
+        ["core.dirman"] = { -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              seceng = "~/neorg/SecEng",
+              thesis = "~/neorg/thesis",
+              private = "~/neorg/private",
+            },
+          },
+        },
       },
     })
   end,
