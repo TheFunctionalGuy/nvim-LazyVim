6fbd1659c611d15facdf29b2cc6f421c5b0587ba
Julian Vesper <jvesper95@gmail.com>
2023-08-31 17:24:07 +0200

    Finalize surroundings

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -34,6 +34,40 @@ return {
         replace = "Mr",
         update_n_lines = "Mn",
       },
+      custom_surroundings = {
+        -- Latex commands: \command{%s}
+        ["C"] = {
+          input = { "\\%w+%b{}", "^.-{().*()}$" },
+          output = function()
+            local fun_name = MiniSurround.user_input("Latex command name")
+            if fun_name == nil then
+              return nil
+            end
+            return { left = ("\\%s{"):format(fun_name), right = "}" }
+          end,
+        },
+        -- Latex environment: \begin{environment}\n%s\n\end{environment}
+        ["E"] = {
+          input = { "\\begin{(%w*)}.-\\end{%1}", "^\\begin{.-}\n().-()\n\\end{.-}$" },
+          output = function()
+            local fun_name = MiniSurround.user_input("Latex environment name")
+            if fun_name == nil then
+              return nil
+            end
+            return { left = ("\\begin{%s}\n"):format(fun_name), right = ("\n\\end{%s}"):format(fun_name) }
+          end,
+        },
+        -- Latex bold text: \textbf{%s}
+        ["B"] = {
+          input = { "\\textbf{%w*}", "^\\textbf{().-()}$" },
+          output = { left = "\\textbf{", right = "}" },
+        },
+        -- Latex italic text: \textit{%s}
+        ["I"] = {
+          input = { "\\textit{%w*}", "^\\textit{().-()}$" },
+          output = { left = "\\textit{", right = "}" },
+        },
+      },
     },
   },
 }
