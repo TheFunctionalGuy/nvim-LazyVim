6d4cafab39deaa56664f37e7197b8df0b28f26f9
Julian Vesper <jvesper95@gmail.com>
2023-09-30 02:09:06 +0200

    Fix indent-blankline plugin
    The options changed in v3 version and needed to get fixed

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,6 +1,6 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   opts = function(_, opts)
-    vim.list_extend(opts.filetype_exclude, { "undotree" })
+    vim.list_extend(opts.exclude.filetypes, { "undotree" })
   end,
 }
