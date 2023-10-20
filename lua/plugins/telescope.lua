c61b2fdff634d809efaaca2bb4fd2c27740d040c
Julian Vesper <jvesper95@gmail.com>
2023-10-20 18:12:33 +0200

    Replace fzf-native by zf-native

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,11 +2,18 @@ return {
   "nvim-telescope/telescope.nvim",
   optional = true,
   dependencies = {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make",
-    config = function()
-      require("telescope").load_extension("fzf")
-    end,
+    -- Use zf as default fuzzy finder here
+    {
+      "natecraddock/telescope-zf-native.nvim",
+      config = function()
+        require("telescope").load_extension("zf-native")
+      end,
+    },
+    -- Disable fzf-native for now
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      enabled = false,
+    },
   },
   opts = {
     defaults = {
