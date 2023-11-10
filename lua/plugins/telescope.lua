52ba5b765794afe59261f9bc958febddea033a61
Julian Vesper <jvesper95@gmail.com>
2023-11-10 23:56:35 +0100

    Add telescope keymaps to cycle history

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,14 +15,14 @@ return {
       enabled = false,
     },
   },
-  opts = {
-    defaults = {
-      mappings = {
-        i = {
-          ["<C-j>"] = "move_selection_next",
-          ["<C-k>"] = "move_selection_previous",
-        },
-      },
-    },
-  },
+  opts = function(_, opts)
+    local actions = require("telescope.actions")
+
+    opts.defaults.mappings.i = vim.tbl_extend("force", opts.defaults.mappings.i, {
+      ["<C-j>"] = actions.move_selection_next,
+      ["<C-k>"] = actions.move_selection_previous,
+      ["<C-a-j>"] = actions.cycle_history_next,
+      ["<C-a-k>"] = actions.cycle_history_prev,
+    })
+  end,
 }
