0e34c4e7d3e69f1b2c8f46c83df5c8117a28dad9
Julian Vesper <julian@radix-security.com>
2024-08-20 15:03:21 +0200

    Add better mappings for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,11 +4,20 @@ return {
   opts = function(_, opts)
     local actions = require("telescope.actions")
 
+    local stopinsert = function()
+      vim.cmd([[stopinsert]])
+    end
+
     opts.defaults.mappings.i = vim.tbl_extend("force", opts.defaults.mappings.i, {
+      -- Better movement
       ["<C-j>"] = actions.move_selection_next,
       ["<C-k>"] = actions.move_selection_previous,
+      -- Better cycling
       ["<C-a-j>"] = actions.cycle_history_next,
       ["<C-a-k>"] = actions.cycle_history_prev,
+      -- Better leaving
+      ["<Esc>"] = actions.close,
+      ["<C-c>"] = stopinsert,
     })
   end,
 }
