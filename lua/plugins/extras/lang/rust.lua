50d61c7117b1d3e87d17d437560f0782579882a5
Julian Vesper <jvesper95@gmail.com>
2025-01-14 13:57:53 +0100

    Rust: disable clippy

--- lua/plugins/extras/lang/rust.lua
+++ lua/plugins/extras/lang/rust.lua
@@ -1,11 +1,22 @@
 return {
-  "neovim/nvim-lspconfig",
-  optional = true,
-  opts = {
-    servers = {
-      taplo = {
-        -- Use local install instead of mason
-        mason = false,
+  {
+    "neovim/nvim-lspconfig",
+    optional = true,
+    opts = {
+      servers = {
+        taplo = {
+          -- Use local install instead of mason
+          mason = false,
+        },
+      },
+    },
+  },
+  {
+    "mrcjkb/rustaceanvim",
+    optional = true,
+    opts = {
+      tools = {
+        enable_clippy = false,
       },
     },
   },
