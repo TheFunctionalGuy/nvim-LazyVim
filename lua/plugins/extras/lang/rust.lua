4f052521436bacca50c9fb45375b54726f9cc1e7
Julian Vesper <julian@montsecure.com>
2025-05-14 10:26:19 +0200

    Rust: make clippy toggleable via env variable

--- lua/plugins/extras/lang/rust.lua
+++ lua/plugins/extras/lang/rust.lua
@@ -14,10 +14,21 @@ return {
   {
     "mrcjkb/rustaceanvim",
     optional = true,
-    opts = {
-      tools = {
-        enable_clippy = false,
-      },
-    },
+    opts = function(_, opts)
+      -- Enable clippy if RUSTACEANVIM_ENABLE_CLIPPY is set in env variables or disable it otherwise
+      if vim.env.RUSTACEANVIM_ENABLE_CLIPPY then
+        return vim.tbl_deep_extend("force", opts or {}, {
+          tools = {
+            enable_clippy = true,
+          },
+        })
+      else
+        return vim.tbl_deep_extend("force", opts or {}, {
+          tools = {
+            enable_clippy = false,
+          },
+        })
+      end
+    end,
   },
 }
