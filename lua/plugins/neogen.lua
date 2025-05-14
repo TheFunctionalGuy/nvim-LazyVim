51a862ca7458f137b23a425b8270c2fbe6b07121
Julian Vesper <jvesper95@gmail.com>
2025-05-14 11:49:54 +0200

    Update neogen.lua

--- lua/plugins/neogen.lua
+++ lua/plugins/neogen.lua
@@ -1,9 +1,8 @@
 return {
   "danymat/neogen",
-  dependencies = { "nvim-treesitter/nvim-treesitter" },
   -- stylua: ignore
   keys = {
     { "<leader>cg", function() require("neogen").generate({}) end, desc = "Generate documentation" },
   },
-  config = true,
+  opts = {},
 }
