eaebaebb7e0fa7c145d68339788c698abc53e41d
Julian Vesper <julian@montsecure.com>
2025-05-14 15:04:13 +0200

    Update dap.lua

--- lua/plugins/extras/dap.lua
+++ lua/plugins/extras/dap.lua
@@ -1,20 +1,23 @@
 return {
-  "mfussenegger/nvim-dap",
-  optional = true,
-  -- stylua: ignore
-  keys = {
-    -- F-keymaps
-    { "<F2>",  function() require("dap").continue() end, desc = "Continue" },
-    { "<F3>",  function() require("dap").step_into() end, desc = "Step Into", },
-    { "<F4>",  function() require("dap").step_over() end, desc = "Step Over", },
-    { "<F5>",  function() require("dap").step_out() end, desc = "Step Out", },
-    { "<F6>",  function() require("dap").step_back() end, desc = "Step Back", },
-    { "<F7>",  function() require("dap").run_last() end, desc = "Restart", },
-    { "<F8>",  function() require("dap").terminate() end, desc = "Terminate", },
-    { "<F9>",  function() require("dap").disconnect() end, desc = "Disconnect", },
-    -- Additional F-keymaps
-    { "<F10>", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint", },
-    { "<F11>", function() require("dap").set_breakpoint(vim.fn.input "[Condition] > ") end, desc = "Conditional Breakpoint", },
-    { "<F12>", function() require("dapui").toggle() end, desc = "Toggle UI", },
+  { import = "lazyvim.plugins.extras.dap.core" },
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    -- stylua: ignore
+    keys = {
+      -- F-keymaps
+      { "<F2>",  function() require("dap").continue() end, desc = "Continue" },
+      { "<F3>",  function() require("dap").step_into() end, desc = "Step Into", },
+      { "<F4>",  function() require("dap").step_over() end, desc = "Step Over", },
+      { "<F5>",  function() require("dap").step_out() end, desc = "Step Out", },
+      { "<F6>",  function() require("dap").step_back() end, desc = "Step Back", },
+      { "<F7>",  function() require("dap").run_last() end, desc = "Restart", },
+      { "<F8>",  function() require("dap").terminate() end, desc = "Terminate", },
+      { "<F9>",  function() require("dap").disconnect() end, desc = "Disconnect", },
+      -- Additional F-keymaps
+      { "<F10>", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint", },
+      { "<F11>", function() require("dap").set_breakpoint(vim.fn.input "[Condition] > ") end, desc = "Conditional Breakpoint", },
+      { "<F12>", function() require("dapui").toggle() end, desc = "Toggle UI", },
+    },
   },
 }
