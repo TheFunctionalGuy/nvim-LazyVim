15cf1a3f1562c51902cf964c38c26d0c4caefb13
Julian Vesper <julian@montsecure.com>
2025-05-14 11:29:18 +0200

    Update telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,6 @@
 return {
   "nvim-telescope/telescope.nvim",
   optional = true,
-  keys = {
-    { "<leader>/", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
-    { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
-  },
   opts = function(_, opts)
     local actions = require("telescope.actions")
 
@@ -12,20 +8,25 @@ return {
       vim.cmd([[stopinsert]])
     end
 
-    opts.defaults.mappings.i = vim.tbl_extend("force", opts.defaults.mappings.i, {
-      -- Better movement
-      ["<C-j>"] = actions.move_selection_next,
-      ["<C-k>"] = actions.move_selection_previous,
-      -- Better cycling
-      ["<C-a-j>"] = actions.cycle_history_next,
-      ["<C-a-k>"] = actions.cycle_history_prev,
-      -- Better leaving
-      ["<Esc>"] = actions.close,
-      ["<C-c>"] = stopinsert,
+    return vim.tbl_deep_extend("force", opts or {}, {
+      defaults = {
+        mappings = {
+          i = {
+            -- Better movement
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+            -- Better cycling
+            ["<C-a-j>"] = actions.cycle_history_next,
+            ["<C-a-k>"] = actions.cycle_history_prev,
+            -- Better leaving
+            ["<Esc>"] = actions.close,
+            ["<C-c>"] = stopinsert,
+          },
+        },
+        -- Custom visuals
+        path_display = { "smart", "filename_first" },
+        layout_strategy = "vertical",
+      },
     })
-
-    -- Custom visuals
-    opts.defaults.path_display = { "smart", "filename_first" }
-    opts.defaults.layout_strategy = "vertical"
   end,
 }
