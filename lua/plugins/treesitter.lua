67e2e6716f764a0a7af4f5321c77170dcb65f9f7
Julian Vesper <jvesper95@gmail.com>
2025-05-17 03:33:47 +0200

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,28 +1,28 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  optional = true,
-  dependencies = {
-    {
-      "nvim-treesitter/nvim-treesitter-context",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      opts.auto_install = true
+
+      vim.list_extend(opts.ensure_installed, {
+        "comment",
+        "diff",
+        "git_config",
+        "git_rebase",
+        "gitattributes",
+        "gitcommit",
+        "jsonc",
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    optional = true,
       -- stylua: ignore
       keys = {
         { "[x", function() require("treesitter-context").go_to_context() end, desc = "Go to context" },
       },
-      config = true,
-    },
+    opts = {},
   },
-  opts = function(_, opts)
-    opts.auto_install = true
-    opts.ignore_install = { "help" }
-
-    vim.list_extend(opts.ensure_installed, {
-      "comment",
-      "diff",
-      "git_config",
-      "git_rebase",
-      "gitattributes",
-      "gitcommit",
-      "jsonc",
-    })
-  end,
 }
