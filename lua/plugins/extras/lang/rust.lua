e9904d4568e7e6e28144b6a641695364a1f8f3dc
Julian Vesper <jvesper95@gmail.com>
2026-01-07 16:57:37 +0100

    Rust: update rust-analyzer config

--- lua/plugins/extras/lang/rust.lua
+++ lua/plugins/extras/lang/rust.lua
@@ -16,20 +16,42 @@ return {
     "mrcjkb/rustaceanvim",
     optional = true,
     opts = function(_, opts)
+      local updated_opts = opts or {}
+
+      updated_opts = vim.tbl_deep_extend("force", updated_opts, {
+        server = {
+          default_settings = {
+            ["rust-analyzer"] = {
+              cargo = {
+                -- Give rust-analyzer own sub-directory in `target`
+                -- This enables running `cargo *` and using the LSP in parallel
+                targetDir = true,
+              },
+              references = {
+                -- Ignore imports when using something like goto references
+                excludeImports = true,
+              },
+            },
+          },
+        },
+      })
+
       -- Enable clippy if RUSTACEANVIM_ENABLE_CLIPPY is set in env variables or disable it otherwise
       if vim.env.RUSTACEANVIM_ENABLE_CLIPPY then
-        return vim.tbl_deep_extend("force", opts or {}, {
+        updated_opts = vim.tbl_deep_extend("force", updated_opts, {
           tools = {
             enable_clippy = true,
           },
         })
       else
-        return vim.tbl_deep_extend("force", opts or {}, {
+        updated_opts = vim.tbl_deep_extend("force", updated_opts, {
           tools = {
             enable_clippy = false,
           },
         })
       end
+
+      return updated_opts
     end,
   },
 }
